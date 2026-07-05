6e63b20c93dd011823b24b82604dd87768862aa6
marzan3698 <marzan3698@gmail.com>
2026-07-05 14:30:21 +0600

    Change image rule to file for logo and favicon validation to support SVG properly

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -154,8 +154,8 @@ class SettingController extends Controller
         $request->validate([
             'site_title' => 'required|string|max:255',
             'site_description' => 'nullable|string|max:500',
-            'site_logo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp|max:10240',
-            'site_favicon' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:10240',
+            'site_logo' => 'nullable|file|mimes:jpeg,png,jpg,gif,svg,webp|max:10240',
+            'site_favicon' => 'nullable|file|mimes:jpeg,png,jpg,gif|max:10240',
         ]);
 
         Setting::updateOrCreate(['key' => 'site_title'], ['value' => $request->site_title]);
